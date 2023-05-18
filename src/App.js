983d03d387806d3d561b5c4f43bd7f31a098d1aa
Eli Conley <elijahtconley@gmail.com>
2023-05-17 22:37:35 -0400

    added animated text and draggable image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "animate.css";
+import SplitText from "./components/SplitText";
+import Draggable from "react-draggable";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Draggable>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </Draggable>
+        <h1>
+          <SplitText copy="Under Construction!" role="heading"></SplitText>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
